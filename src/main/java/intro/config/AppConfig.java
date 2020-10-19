c6303ccb6531ef6921c170cbaf4e8f4e6b27c9fd
jlightgreen <ShvabovichJulia@gmail.com>
2020-10-19 15:12:26 +0300

    spring-1

---
+++ src/main/java/intro/config/AppConfig.java
@@ -0,0 +1,40 @@
+package intro.config;
+
+import intro.model.User;
+import java.util.Properties;
+import javax.sql.DataSource;
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+
+@Configuration
+@ComponentScan(basePackages = {
+        "intro.service",
+        "intro.dao"
+})
+public class AppConfig {
+    @Bean
+    public DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/spring?serverTimezone=UTC");
+        dataSource.setUsername("root");
+        dataSource.setPassword("12345");
+        return dataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean getSessionFactory() {
+        LocalSessionFactoryBean factoryBean = new LocalSessionFactoryBean();
+        factoryBean.setDataSource(getDataSource());
+        Properties properties = new Properties();
+        properties.put("hibernate.show_sql", "true");
+        properties.put("hibernate.format_sql", "true");
+        properties.put("hibernate.hbm2ddl.auto", "create-drop");
+        factoryBean.setHibernateProperties(properties);
+        factoryBean.setAnnotatedClasses(User.class);
+        return factoryBean;
+    }
+}
---
+++ src/main/java/intro/dao/UserDao.java
@@ -0,0 +1,11 @@
+package intro.dao;
+
+import intro.model.User;
+import java.util.List;
+
+public interface UserDao {
+    void add(User user);
+
+    List<User> getAll();
+
+}

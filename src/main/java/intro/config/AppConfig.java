770a76cfb809d79879880fc617ac3b891e7b9cfb
jlightgreen <ShvabovichJulia@gmail.com>
2020-10-20 02:08:26 +0300

    spring-2

--- src/main/java/intro/config/AppConfig.java
+++ src/main/java/intro/config/AppConfig.java
@@ -40,7 +40,6 @@ public class AppConfig {
         factoryBean.setDataSource(getDataSource());
         Properties properties = new Properties();
         properties.put("hibernate.show_sql", environment.getProperty("hibernate.show_sql"));
-        properties.put("hibernate.format_sql", environment.getProperty("hibernate.format_sql"));
         properties.put("hibernate.hbm2ddl.auto", environment.getProperty("hibernate.hbm2ddl.auto"));
         factoryBean.setHibernateProperties(properties);
         factoryBean.setAnnotatedClasses(User.class);

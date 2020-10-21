eeec74f31976979fa4af9c6eb133c5699bb936ba
jlightgreen <ShvabovichJulia@gmail.com>
2020-10-21 09:43:51 +0300

    Spring MVC

--- src/main/java/intro/controller/UserController.java
+++ src/main/java/intro/controller/UserController.java
@@ -1,12 +1,10 @@
 package intro.controller;
 
-import intro.config.AppConfig;
 import intro.dto.UserResponseDto;
 import intro.model.User;
 import intro.service.UserService;
 import java.util.List;
 import java.util.stream.Collectors;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,31 +13,33 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    AnnotationConfigApplicationContext context =
-            new AnnotationConfigApplicationContext(AppConfig.class);
-    UserService userService = context.getBean(UserService.class);
+    private final UserService userService;
 
-    @GetMapping(value = "/inject")
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/inject")
     public void injectUsers() {
         userService.add(new User("test@gmail.com", "123"));
         userService.add(new User("one_more_test@gmail.com", "321"));
     }
 
-    @GetMapping(value = "/{userId}")
+    @GetMapping("/{userId}")
     public UserResponseDto get(@PathVariable Long userId) {
-        UserResponseDto userDto = mapUserResponseDto(userService.getById(userId));
+        UserResponseDto userDto = mapUserDto(userService.getById(userId));
         return userDto;
     }
 
     @GetMapping
     public List<UserResponseDto> getAll() {
         List<UserResponseDto> userDtos = userService.listUsers().stream()
-                .map(user -> mapUserResponseDto(user))
+                .map(user -> mapUserDto(user))
                 .collect(Collectors.toList());
         return userDtos;
     }
 
-    private UserResponseDto mapUserResponseDto(User user) {
+    private UserResponseDto mapUserDto(User user) {
         UserResponseDto userResponseDto = new UserResponseDto();
         userResponseDto.setEmail(user.getEmail());
         userResponseDto.setPassword(user.getPassword());
